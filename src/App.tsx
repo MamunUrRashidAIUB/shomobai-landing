3098f4a08e9b6874cfa72e151a592d921eec8048
MamunUrRashidAIUB <md.mamun.ur.rashid.cse@gmail.com>
2026-06-11 18:43:09 +0600

    some fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-
 import './App.css'
 import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
 import Services from './components/services/Services'
 import About from './components/about/About'
 import Notice from './components/notice/Notice'
-
 function App() {
   return (
     <>
@@ -15,6 +12,7 @@ function App() {
       <Services />
       <About />
       <Notice />
+    
 
     </>
   )
